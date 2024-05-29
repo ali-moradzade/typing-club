39c6a986b4e374c9cdbe495ec3107b7ae6e793c2
Ali Moradzade <alimorizz1379@gmail.com>
2024-05-29 19:01:46 +0330

    removing one-off bug when typing content

--- cypress-automation/cypress/support/commands.js
+++ cypress-automation/cypress/support/commands.js
@@ -17,10 +17,14 @@ Cypress.Commands.add('writeLesson', (lessonName, content) => {
                     force: true
                 });
 
-                cy.get('.inview').click({force: true}).type(content.slice(0, -1));
+                // Type all the content except for last char, type it manually in the website to tune speed
+                // (speed > 150 is denied by typingclub)
+                cy.get('.inview').type(`{backspace}${content.slice(0, -1)}`, {
+                    delay: 0,
+                });
 
                 cy.pause();
-                cy.visit('https://www.typingclub.com/sportal/')
+                cy.visit('https://www.typingclub.com/sportal/');
             }
         });
 });
